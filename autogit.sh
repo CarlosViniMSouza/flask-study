b835ca51244e58cf023da8f4efb3320f0584722e
Carlos Vinicius <vinicius.souza5530@gmail.com>
2022-08-20 00:58:48 +0000

    Problems in Codespace and Gitpod | Continue the project on PC

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Problems in Codespace | setting templates and static folders"
+git commit -m "Problems in Codespace and Gitpod | Continue the project on PC"
 git push
 git status
